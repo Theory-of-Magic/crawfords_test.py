986cb5230fc20bbe4649e2583dd82ae7ea1e98bf
MacSlash <104305464+MacSlash@users.noreply.github.com>
2022-06-14 19:08:49 +0100

    Add files via upload

--- src/crawfords_test.py
+++ src/crawfords_test.py
@@ -1,1 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+modifier = 0
+dicetype = int(input(print("How many sides does your dice have?")))
+rolls = int(input(print("How many rolls?")))
+PI = 3.1415926535897932384626433832795
 
+A = (0.5(dicetype + 1) * rolls + modifier ) * ((PI / 6)(dicetype ^ 2 - 1 ))^ -(1/2)
+sigma = np.sqrt((rolls * (dicetype ^ 2 - 1)) / 12) 
