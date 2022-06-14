7c53408b4a1a81d45e4ffcf86ae18b0604496e89
MacSlash <104305464+MacSlash@users.noreply.github.com>
2022-06-14 21:14:10 +0100

    Update crawfords_test.py

--- src/crawfords_test.py
+++ src/crawfords_test.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 modifier = float(0)
-dicetype = float(input(print("How many sides does your dice have?")))
 rolls = float(input(print("How many rolls?")))
+dicetype = float(input(print("How many sides does your dice have?")))
 PI = 3.1415926535897932384626433832795
 
 A = ((0.5 * (dicetype + 1.0) * rolls + modifier ) * (((PI / 6.0) * rolls) * (dicetype ** 2.0 - 1.0 ))** -0.5)
